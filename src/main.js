99fea28880a3e890698aa6ce22eac470c10be999
reaton333 <rosseaton92@gmail.com>
2021-08-04 21:44:43 -0400

    First commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader';
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = [/ion-\w*/];
+
+defineIonPhaser(window);
 
 new Vue({
   render: h => h(App),
